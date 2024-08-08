8e9102cadd5f2a2aabe6a017bb4986deb685eb07
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-08-08 22:37:49 +0530

    functionality added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,160 +1,172 @@
 "use client";
-import Image from 'next/image';
+import Image from "next/image";
+import ThemeToggle from "@/components/ThemeToggle";
 
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/NavigationMenu";
 import { tools } from "@/constants";
 import { cn } from "@/lib/utils";
 import { usePricingModal } from "@/store";
 import { SignInButton, SignUpButton, useAuth } from "@clerk/nextjs";
 import Link from "next/link";
-import React from 'react';
+import React from "react";
 import MobileNavbar from "./MobileNavbar";
 import { Button } from "./ui/Button";
 import { Skeleton } from "./ui/Skeleton";
 import UserAccountNav from "./user/UserAccountNav";
 
 const Navbar = () => {
+  const { isLoaded, isSignedIn } = useAuth();
 
-    const { isLoaded, isSignedIn } = useAuth();
+  const { isOpen, setIsOpen } = usePricingModal();
 
-    const { isOpen, setIsOpen } = usePricingModal();
+  return (
+    <header className="sticky inset-x-0 top-0 z-50 w-full border-b h-14 bg-white/50 backdrop-blur-md border-border">
+      <div className="items-center justify-between hidden w-full h-full px-4 mx-auto max-w-screen-xl md:flex sm:px-8">
+        <div className="flex items-center gap-x-8">
+          <div className="flex items-center justify-center gap-x-2">
+            <Link href="/" className="flex items-center select-none gap-x-2">
+              <Image
+                src="/icon.png"
+                width={80}
+                height={80}
+                alt="Imo Logo"
+                className="object-cover w-auto h-12 transition-all duration-700 ease-out dark:hidden"
+              />
 
-    return (
-        <header className="sticky inset-x-0 top-0 z-50 w-full border-b h-14 bg-white/50 backdrop-blur-md border-border">
-            <div className="items-center justify-between hidden w-full h-full px-4 mx-auto max-w-screen-xl md:flex sm:px-8">
+              <Image
+                src="/profile.ico"
+                width={80}
+                height={80}
+                alt="Imo Logo"
+                className="object-cover w-auto h-20 transition-all duration-700 ease-out dark:hidden"
+              />
+            </Link>
+          </div>
+          
 
-                <div className="flex items-center gap-x-8">
-                    <div className="flex items-center justify-center gap-x-2">
-                        <Link href="/" className="flex items-center select-none gap-x-2">
-                        <Image src="/icon.png" width={80} height={80} alt="Imo Logo" className="object-cover w-auto h-12 transition-all duration-700 ease-out dark:hidden" />
+          <NavigationMenu className="hidden md:flex">
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Tools</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                    {tools.map((tool) => (
+                      <ListItem key={tool.id} title={tool.name} href="#">
+                        {tool.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/about" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    About
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+              <NavigationMenuItem>
+                <Link href="/pricing" legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Pricing
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
 
-                        <Image src="/profile.ico" width={80} height={80} alt="Imo Logo" className="object-cover w-auto h-20 transition-all duration-700 ease-out dark:hidden" />
-                        </Link>
-                    </div>
-
-                    <NavigationMenu className="hidden md:flex">
-                        <NavigationMenuList>
-                            <NavigationMenuItem>
-                                <NavigationMenuTrigger>
-                                    Tools
-                                </NavigationMenuTrigger>
-                                <NavigationMenuContent>
-                                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                                        {tools.map((tool) => (
-                                            <ListItem
-                                                key={tool.id}
-                                                title={tool.name}
-                                                href="#"
-                                            >
-                                                {tool.description}
-                                            </ListItem>
-                                        ))}
-                                    </ul>
-                                </NavigationMenuContent>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <Link href="/about" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        About
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                            <NavigationMenuItem>
-                                <Link href="/pricing" legacyBehavior passHref>
-                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                        Pricing
-                                    </NavigationMenuLink>
-                                </Link>
-                            </NavigationMenuItem>
-                        </NavigationMenuList>
-                    </NavigationMenu>
+        <div className="flex items-center justify-end">
+          {isSignedIn ? (
+            <div className="flex items-center justify-end gap-x-4">
+              <Button
+                size="sm"
+                variant="ghost"
+                className="pro text-rose-500 hover:bg-transparent"
+                onClick={() => setIsOpen(true)}
+              >
+                Go Pro
+              </Button>
+              <div className="h-8 w-[1.5px] bg-neutral-200"></div>
+              {isLoaded ? (
+                <UserAccountNav />
+              ) : (
+                <div className="flex items-center justify-end gap-x-4">
+                  <Skeleton className="w-20 h-8" />
+                  <Skeleton className="w-8 h-8 rounded-full" />
                 </div>
-
-                <div className="flex items-center justify-end">
-                    {isSignedIn ? (
-                        <div className="flex items-center justify-end gap-x-4">
-                            <Button size="sm" variant="ghost" className="pro text-rose-500 hover:bg-transparent" onClick={() => setIsOpen(true)}>
-                                Go Pro
-                            </Button>
-                            <div className="h-8 w-[1.5px] bg-neutral-200"></div>
-                            {isLoaded ? (
-                                <UserAccountNav />
-                            ) : (
-                                <div className="flex items-center justify-end gap-x-4">
-                                    <Skeleton className="w-20 h-8" />
-                                    <Skeleton className="w-8 h-8 rounded-full" />
-                                </div>
-                            )}
-                        </div>
-                    ) : (
-                        <div className="flex items-center justify-end gap-x-4">
-                            <Button size="sm" variant="ghost" className="pro text-rose-500 hover:bg-transparent" onClick={() => setIsOpen(true)}>
-                                Go Pro
-                            </Button>
-                            <div className="h-8 w-[1.5px] bg-neutral-200"></div>
-                            {isLoaded ? (
-                                <div className="flex items-center gap-x-4">
-                                    <SignInButton mode="modal">
-                                        <Button size="sm" variant="ghost">
-                                            Sign in
-                                        </Button>
-                                    </SignInButton>
-                                    <SignUpButton mode="modal">
-                                        <Button size="sm">
-                                            Sign up
-                                        </Button>
-                                    </SignUpButton>
-                                </div>
-                            ) : (
-                                <div className="flex items-center justify-end gap-x-4">
-                                    <Skeleton className="w-20 h-8" />
-                                    <Skeleton className="w-20 h-8" />
-                                </div>
-                            )}
-                        </div>
-                    )}
-                </div>
-
+              )}
             </div>
+          ) : (
+            <div className="flex items-center justify-end gap-x-4">
+              <Button
+                size="sm"
+                variant="ghost"
+                className="pro text-rose-500 hover:bg-transparent"
+                onClick={() => setIsOpen(true)}
+              >
+                Go Pro
+              </Button>
+              <div className="h-8 w-[1.5px] bg-neutral-200"></div>
+              {isLoaded ? (
+                <div className="flex items-center gap-x-4">
+                  <SignInButton mode="modal">
+                    <Button size="sm" variant="ghost">
+                      Sign in
+                    </Button>
+                  </SignInButton>
+                  <SignUpButton mode="modal">
+                    <Button size="sm">Sign up</Button>
+                  </SignUpButton>
+                </div>
+              ) : (
+                <div className="flex items-center justify-end gap-x-4">
+                  <Skeleton className="w-20 h-8" />
+                  <Skeleton className="w-20 h-8" />
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      </div>
 
-            <MobileNavbar />
-
-        </header>
-    )
+      <MobileNavbar />
+    </header>
+  );
 };
 
 const ListItem = React.forwardRef<
-    React.ElementRef<"a">,
-    React.ComponentPropsWithoutRef<"a">
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
 >(({ className, title, children, ...props }, ref) => {
-    return (
-        <li>
-            <NavigationMenuLink asChild>
-                <a
-                    ref={ref}
-                    className={cn(
-                        "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-                        className
-                    )}
-                    {...props}
-                >
-                    <div className="text-sm font-medium leading-none">{title}</div>
-                    <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
-                        {children}
-                    </p>
-                </a>
-            </NavigationMenuLink>
-        </li>
-    )
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  );
 });
 ListItem.displayName = "ListItem";
 
-export default Navbar
+export default Navbar;
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
-    title: "Flint - Shape Your Files with Creative Conversion",
-    description: "Flint is a file conversion tool that allows you to convert files in a creative way. It supports a wide range of file formats and allows you to convert files in a creative way.",
+    title: "Imo - it's simple",
+    description: "Imo is a web application that basically used to convert different types of image files.",
     icons: {
         icon: [
             {
