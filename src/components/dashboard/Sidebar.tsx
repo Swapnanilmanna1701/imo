e77cd223cecd9dadb26f68907b7e6c1af0187f81
Swapnanilmanna1701 <sm.22u10337@btech.nitdgp.ac.in>
2024-08-13 01:47:00 +0530

    icon added

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -2,16 +2,31 @@ import { Button } from "@/components/ui/Button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/Card";
 import { Bell, File, Home, Settings } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
     return (
         <div className="hidden border-r bg-black lg:block">
             <div className="flex h-full max-h-screen flex-col gap-2">
                 <div className="flex h-[60px] items-center px-6">
-                    <Link className="flex items-center font-bold" href="/">
-                        Flint
-                    </Link>
-                    <Button className="ml-auto h-8 w-8" size="icon" variant="outline">
+                <Link href="/" className="flex items-center select-none gap-x-2">
+              <Image
+                src="/icon.png"
+                width={80}
+                height={80}
+                alt="Imo Logo"
+                className="object-cover w-auto h-12 transition-all duration-700 ease-out dark:hidden"
+              />
+
+              <Image
+                src="/profile.ico"
+                width={80}
+                height={80}
+                alt="Imo Logo"
+                className="object-cover w-auto h-20 transition-all duration-700 ease-out dark:hidden"
+              />
+            </Link>
+                    <Button className="ml-auto h-8 w-8 text-black pro" size="icon" variant="outline">
                         <Bell className="h-4 w-4" />
                         <span className="sr-only">Toggle notifications</span>
                     </Button>
